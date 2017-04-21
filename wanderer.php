342efe475d9af59f6725c3c424c144eed073033e
Ramya <rsrirama@andrew.cmu.edu>
2017-04-21 19:59:12 -0400

    Major changes to Wanderer interface

--- wanderer.php
+++ wanderer.php
@@ -3,95 +3,126 @@
 <head>
 </head>
 <body>
-  <input id="section-id" type="text" placeholder="Section ID" />
-
-  <br><br>
-  <div id="sse">
-    <a id="getaggregate" type="button" href="#">Get Aggregate!</a>
-    <br><br>
-    <p id="status" >Keep going!</p>
+<!--<input id="section-id" type="text" placeholder="Section ID" />-->
+<div id="sse">
+<a id="getaggregate" type="button" href="#">Get Aggregate!</a>
+<br>
+  <div>
+  <p id="flag1" style="color:black;font-size:12px;font-weight:bold"></p> 
   </div>
-  
+  <div>
+  <img id="flagstatus1" width="30" height="30" style="background-color:white" >
+  </div>
+  <div >
+  <p id="flag2" style="color:black;font-size:12px;font-weight:bold"></p> 
+  </div>
+  <div>
+  <img id="flagstatus2" width="30" height="30" style="background-color:white" >
+  </div>
+  <div >
+  <p id="flag3" style="color:black;font-size:12px;font-weight:bold"></p> 
+  </div>
+  <div>
+  <img id="flagstatus3" width="30" height="30" style="background-color:white" >
+  </div>
+</div>
+<script type="text/javascript">
+      if ("WebSocket" in window) {
+      <?php
+      $treasurehunt_host = isset($_ENV['TREASUREHUNT_HOST']) ? $_ENV['TREASUREHUNT_HOST'] : 'localhost';
+      $treasurehunt_port = isset($_ENV['TREASUREHUNT_PORT']) ? $_ENV['TREASUREHUNT_PORT'] : 9000;
+      echo "const url = 'ws://${treasurehunt_host}:${treasurehunt_port}';\n";
+      ?>
 
-  <script type="text/javascript">
-  if ("WebSocket" in window) {
-    <?php
-    $treasurehunt_host = isset($_ENV['TREASUREHUNT_HOST']) ? $_ENV['TREASUREHUNT_HOST'] : 'localhost';
-    $treasurehunt_port = isset($_ENV['TREASUREHUNT_PORT']) ? $_ENV['TREASUREHUNT_PORT'] : 9000;
-    echo "const url = 'ws://${treasurehunt_host}:${treasurehunt_port}';\n";
-    ?>
-
-    const getAggregate = function () {
-      // Update the wanderer state.
-      const updateWanderer = function () {
-         document.body.style.backgroundColor = "white";
-        const sectionIdInput = document.getElementById('section-id');
-        if (ws.readyState === ws.OPEN) {
-          var msg = {
-              section: parseInt(sectionIdInput.value) - 1,
-              slection: 3,
-              method: "getFromWanderer"
-          };
-          ws.send(JSON.stringify(msg));
-        }
-      };
-
-      // Request for updates every 1 second.
-      var intervalID = setInterval(updateWanderer, 1000);
-
+      const getAggregate = function () {
+        
       // Create a WebSocket.
       var ws = new WebSocket(url);
       ws.onopen = function()
       {
-        // Web Socket is connected, send data using send()
-        console.log('ws.onopen');
-        updateWanderer();
+       // console.log('ws.onopen');
       };
                         
       ws.onmessage = function (evt) 
       { 
-      if (evt.data == "plus10warmer" || evt.data == "plus10colder" || evt.data == "plus10plant" ) console.log("Ignoring the server side aggregate broadcast");
-      else if ( evt.data == "flag1correct" || evt.data == "flag2correct" || evt.data == "flag3correct")
+      if (evt.data == "plus10warmer" || evt.data == "plus10colder" || evt.data == "plus10marker" ) 
       {
-        console.log(evt.data);
-        document.getElementById('status').innerHTML= "GOOD JOB! Go for the next one!";
-      }
-      else if( evt.data == "flag1wrong" || evt.data == "flag2wrong" || evt.data == "flag3wrong")
-      {
-        console.log(evt.data);
-        document.getElementById('status').innerHTML="You dropped the marker at the wrong place! TRY AGAIN!";
-      }
+        console.log("Ignoring the server side aggregate broadcast");
+      }//ifplusten
       else
+      {   
+      var msg = evt.data.split(","); 
+      if(msg.includes("wanderer"))
       {
-        
-      //document.getElementById('status').innerHTML="Keep Going!";
-        
-      var msg = evt.data.split(" "); 
-      console.log(evt.data);
-
-      if(msg[0] > msg[1] && msg[0] > msg[2])
+      if(msg[1] > msg[2] && msg[1] > msg[3])
             {       
-                  console.log("red");  
+                 
                   document.body.style.backgroundColor = "red";
             }
-      else if(msg[1] > msg[0] && msg[1] > msg[2])
+      else if(msg[2] > msg[1] && msg[2] > msg[3])
             {       
-                  console.log("blue");          
-                  document.body.style.backgroundColor = "blue";
-            }
-      else if(msg[2] > msg[0] && msg[2] > msg[1])
-            {       
-                  console.log("yellow");        
+                    
                   document.body.style.backgroundColor = "yellow";
             }
-	 else
-	 {
-		 console.log("white");        
-         document.body.style.backgroundColor = "white";
-	 }
+      else if(msg[3] > msg[1] && msg[3] > msg[2])
+            {       
+                        
+                  document.body.style.backgroundColor = "lightblue";
+            }
+	    else {}
+      } //ifwanderer
+      else
+      {
+        //{"event":"state","game_on":true,"flag1":true,"flag2":true,"flag3":false,"game_off":false}
+      var page = JSON.parse(evt.data);
+      console.log(page.game_on,page.flag1,page.flag2,page.flag3,page.game_off);
+      if(page.game_on===true)
+      {
+      
+      if(page.flag1===true && page.flag2===false && page.flag3===false)
+      {
+         document.getElementById('flag1').innerHTML="Flag 1";
+         document.getElementById('flagstatus1').src="../images/approved.png";
       }
-		  
-
+      if(page.flag1===false && page.flag2===false && page.flag3===false)
+      {
+        document.getElementById('flag1').innerHTML="Flag 1";
+       document.getElementById('flagstatus1').src="../images/declined.png";
+      }
+      if(page.flag2===true && page.flag1===true && page.flag3===false)
+      {
+         document.getElementById('flag2').innerHTML="Flag 2";
+         document.getElementById('flagstatus2').src="../images/approved.png";
+      }
+      if(page.flag2===false && page.flag1===true && page.flag3===false)
+      {
+         document.getElementById('flag2').innerHTML="Flag 2";
+         document.getElementById('flagstatus2').src="../images/declined.png";
+      }
+      if(page.flag3===true && page.flag2===true && page.flag1===true)
+      {
+         document.getElementById('flag3').innerHTML="Flag 3";
+         document.getElementById('flagstatus3').src="../images/approved.png";
+      }
+      if(page.flag3===false && page.flag2===true && page.flag1===true)
+      {
+         document.getElementById('flag3').innerHTML="Flag 3";
+         document.getElementById('flagstatus3').src="../images/declined.png";
+      }
+      
+      }//ifgameon 
+      if(page.game_off)
+      {
+        document.getElementById('flag1').innerHTML="";
+        document.getElementById('flagstatus1').src="";
+        document.getElementById('flag2').innerHTML="";
+        document.getElementById('flagstatus2').src="";
+        document.getElementById('flag3').innerHTML="";
+        document.getElementById('flagstatus3').src=""; 
+      }//ifgameoff
+      } //elsewanderer
+      } //elseplusten
+       
       };
                         
       ws.onclose = function()
@@ -103,14 +134,12 @@
 
     const getAggregateBtn = document.getElementById('getaggregate');
     getAggregateBtn.addEventListener('click', getAggregate);
-  }
-  
-  else
-  {
+    }
+    else
+    {
         // The browser doesn't support WebSocket
         alert("WebSocket NOT supported by your Browser!");
-  }
-  </script>
-      
+    }
+</script>    
 </body>
 </html>
